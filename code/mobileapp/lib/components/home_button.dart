606574a4696e194618437eca8784dfa2de979e6d
Sem VB <sem.van.broekhoven@gmail.com>
2025-11-27 15:48:08 +0100

    remove redundant function in widget build fn

--- code/mobileapp/lib/components/home_button.dart
+++ code/mobileapp/lib/components/home_button.dart
@@ -21,58 +21,54 @@ class HomeButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    GestureDetector homeButton() {
-      return GestureDetector(
-          onTap: () {
-            if (route.substring(0, 5) == "https") {
-              launchUrlString(route);
-              return;
-            }
-            Navigator.pushNamed(
-              context,
-              route,
-              arguments: <String, dynamic>{
-                'sectionId': sectionId,
-                'route': route,
-              },
-            );
+    return GestureDetector(
+      onTap: () {
+        if (route.substring(0, 5) == "https") {
+          launchUrlString(route);
+          return;
+        }
+        Navigator.pushNamed(
+          context,
+          route,
+          arguments: <String, dynamic>{
+            'sectionId': sectionId,
+            'route': route,
           },
-          child: Container(
-            margin: const EdgeInsets.all(0.0),
-            padding: const EdgeInsets.all(18.0),
-            decoration: BoxDecoration(
-                color: Colors.white.withAlpha(64),
-                borderRadius: const BorderRadius.all(Radius.circular(30))),
-            child: FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Column(
-                children: [
-                  FaIcon(
-                    icon,
-                    color: iconColor,
-                    size: 82,
-                  ),
-                  const SizedBox(height: 15), // padding tussen icon en tekst
-                  Text(
-                    name,
-                    style: GoogleFonts.poppins(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                        shadows: [
-                          const Shadow(
-                            blurRadius: 5,
-                            color: Colors.black45,
-                            offset: Offset(0, 2),
-                          )
-                        ]),
-                  ),
-                ],
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.all(0.0),
+        padding: const EdgeInsets.all(18.0),
+        decoration: BoxDecoration(
+          color: Colors.white.withAlpha(64),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Column(
+            children: [
+              FaIcon(
+                icon,
+                color: iconColor,
+                size: 82,
               ),
-            ),
-          ));
-    }
-
-    return homeButton();
+              const SizedBox(height: 15), // padding tussen icon en tekst
+              Text(
+                name,
+                style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white, shadows: [
+                  const Shadow(
+                    blurRadius: 5,
+                    color: Colors.black45,
+                    offset: Offset(0, 2),
+                  )
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
