63acdd811cae35b1c82d2c0ab236cc9b0ece48c5
Sem VB <sem.van.broekhoven@gmail.com>
2025-11-28 11:18:02 +0100

    change custom button impl to button widget

--- code/mobileapp/lib/pages/home.dart
+++ code/mobileapp/lib/pages/home.dart
@@ -51,108 +51,113 @@ class _HomeState extends State<Home> {
     final double logoVerticalOffset = screenHeight * 0.01;
 
     return Scaffold(
-        body: Container(
-            color: const Color(0xFF46ae93),
-            child: Container(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: logoVerticalOffset),
-                      child: Center(
-                        child: SvgPicture.asset(
-                          'assets/WAAIBURG_DEFINITIEF_vector_RGB_Tekengebied 1-01.svg',
-                          color: Colors.white,
-                          semanticsLabel: 'Waaiburg Logo',
-                          height: 150,
+      body: Container(
+        color: const Color(0xFF46ae93),
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: logoVerticalOffset),
+                child: Center(
+                  child: SvgPicture.asset(
+                    'assets/WAAIBURG_DEFINITIEF_vector_RGB_Tekengebied 1-01.svg',
+                    colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                    semanticsLabel: 'Waaiburg Logo',
+                    height: 150,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 18,
+                    childAspectRatio: 1,
+                    primary: false,
+                    shrinkWrap: false,
+                    children: [
+                      const HomeButton(
+                        name: "JONGEREN",
+                        icon: FontAwesomeIcons.child,
+                        iconColor: Color(0xFF3855a2),
+                        sectionId: 2,
+                        route: "/infosegment",
+                      ),
+                      const HomeButton(
+                        name: "VOLWASSENEN",
+                        icon: FontAwesomeIcons.userTie,
+                        iconColor: Color(0xBBFFFFFF),
+                        sectionId: 1,
+                        route: "/infosegment",
+                      ),
+                      const HomeButton(
+                        name: "NIEUWTJES",
+                        icon: FontAwesomeIcons.newspaper,
+                        iconColor: Color(0xBBFFFFFF),
+                        sectionId: 3,
+                        route: "/news",
+                      ),
+                      const HomeButton(
+                        name: "WEBSITE",
+                        icon: FontAwesomeIcons.globe,
+                        iconColor: Color(0xFF3855a2),
+                        sectionId: 1,
+                        route: "https://www.dewaaiburg.be/",
+                      ),
+                      if (userLoggedIn) ...[
+                        const HomeButton(
+                          name: "LEVENSBOOM",
+                          icon: FontAwesomeIcons.tree,
+                          iconColor: Color(0xFF3855a2),
+                          sectionId: 1,
+                          route: "/treehome",
                         ),
+                        const HomeButton(
+                          name: "GEGEVENS",
+                          icon: FontAwesomeIcons.user,
+                          iconColor: Color(0xFF3855a2),
+                          sectionId: 1,
+                          route: "/userdetails",
+                        ),
+                      ],
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                  width: screenHeight,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(18.0),
+                      backgroundColor: Colors.white.withAlpha(64),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
                       ),
                     ),
-                    Expanded(
-                      child: SizedBox(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        child: GridView.count(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 20,
-                          crossAxisSpacing: 18,
-                          childAspectRatio: 1,
-                          primary: false,
-                          shrinkWrap: false,
-                          children: [
-                            const HomeButton(
-                              name: "JONGEREN",
-                              icon: FontAwesomeIcons.child,
-                              iconColor: Color(0xFF3855a2),
-                              sectionId: 2,
-                              route: "/infosegment",
-                            ),
-                            const HomeButton(
-                              name: "VOLWASSENEN",
-                              icon: FontAwesomeIcons.userTie,
-                              iconColor: Color(0xBBFFFFFF),
-                              sectionId: 1,
-                              route: "/infosegment",
-                            ),
-                            const HomeButton(
-                              name: "NIEUWTJES",
-                              icon: FontAwesomeIcons.newspaper,
-                              iconColor: Color(0xBBFFFFFF),
-                              sectionId: 3,
-                              route: "/news",
-                            ),
-                            const HomeButton(
-                              name: "WEBSITE",
-                              icon: FontAwesomeIcons.globe,
-                              iconColor: Color(0xFF3855a2),
-                              sectionId: 1,
-                              route: "https://www.dewaaiburg.be/",
-                            ),
-                            if (userLoggedIn) ...[
-                              const HomeButton(
-                                name: "LEVENSBOOM",
-                                icon: FontAwesomeIcons.tree,
-                                iconColor: Color(0xFF3855a2),
-                                sectionId: 1,
-                                route: "/treehome",
-                              ),
-                              const HomeButton(
-                                name: "GEGEVENS",
-                                icon: FontAwesomeIcons.user,
-                                iconColor: Color(0xFF3855a2),
-                                sectionId: 1,
-                                route: "/userdetails",
-                              ),
-                            ],
-                          ],
-                        ),
-                      ),
+                    onPressed: () {
+                      userLoggedIn ? logOut() : Navigator.pushNamed(context, '/login');
+                    },
+                    child: Text(
+                      userLoggedIn ? 'Uitloggen' : 'Inloggen',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white, shadows: [
+                        Shadow(
+                          blurRadius: 5,
+                          color: Colors.black45,
+                          offset: Offset(0, 2),
+                        )
+                      ]),
                     ),
-                    SizedBox(
-                      width: screenHeight,
-                      child: GestureDetector(
-                          onTap: () {
-                            userLoggedIn ? logOut() : Navigator.pushNamed(context, '/login');
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.all(5.0),
-                            padding: const EdgeInsets.all(18.0),
-                            decoration: BoxDecoration(color: Colors.white.withAlpha(64), borderRadius: const BorderRadius.all(Radius.circular(30))),
-                            child: Text(
-                              userLoggedIn ? 'Uitloggen' : 'Inloggen',
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white, shadows: [
-                                Shadow(
-                                  blurRadius: 5,
-                                  color: Colors.black45,
-                                  offset: Offset(0, 2),
-                                )
-                              ]),
-                            ),
-                          )),
-                    ),
-                  ],
-                ))));
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
