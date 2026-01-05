448cc981b84603ff8b8858e45851560d4f6133d4
Sem VB <sem.van.broekhoven@gmail.com>
2026-01-05 15:55:03 +0100

    update padding on input widgets in input container

--- code/mobileapp/lib/screens/tree_refactor/widgets/completion_card.dart
+++ code/mobileapp/lib/screens/tree_refactor/widgets/completion_card.dart
@@ -6,6 +6,7 @@ class CompletionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(0),
       color: Theme.of(context).colorScheme.tertiary,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
